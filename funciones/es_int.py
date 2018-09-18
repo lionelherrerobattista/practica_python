83ac0e41cf68ce4b6b7b6fc1f3a594f82829a728
Lionel Herrero Battista <->
2018-09-18 20:10:52 -0300

    Funciones

---
+++ funciones/es_int.py
@@ -0,0 +1,7 @@
+#Verifica si x es un numero entero
+def es_entero(x):
+	#Si dividimos a x por 1 y es entero el resto == 0
+	if x % 1 == 0:
+	return True
+	else:
+	return False
---
+++ funciones/es_par.py
@@ -0,0 +1,7 @@
+#Comprueba si x es par
+def es_par(x):
+	# x / 2 tiene que dar resto == 0
+	if x % 2 == 0:
+	return True
+	else:
+	return False
