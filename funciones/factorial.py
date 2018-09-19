fbe71396d04e1c575f3eba54f840e7027f82da93
Lionel Herrero Battista <->
2018-09-19 12:40:57 -0300

    Funciones

--- funciones/factorial.py
+++ funciones/factorial.py
@@ -1,8 +1,14 @@
 #Función que calcula el factorial de un numero 
 #y devuelve el factorial
 def factorial(x):
-  factorial = x
-  #range se ejecuta hasta x-1
-  for i in range(1, x):
-    factorial = factorial * (x - i) #uso x -1 en lugar de factorial (sino cambia valor de x)
-  return factorial
+	factorial = x
+	#range se ejecuta hasta x-1
+	if x != 0:
+	  for i in range(1, x):
+		  factorial = factorial * (x - i) #uso x -1 en lugar de factorial (sino cambia valor de x)
+	else:
+	  factorial = 1
+	  
+	return factorial
+  
+print (factorial(0))
