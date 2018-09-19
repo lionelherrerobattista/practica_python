35751f317a33c254feabdbde0e198f24e07ce21d
Lionel Herrero Battista <->
2018-09-19 13:37:33 -0300

    Funciones

--- funciones/girar_texto.py
+++ funciones/girar_texto.py
@@ -1,5 +1,5 @@
 #Función que devuelve el texto que recibe al revés
-def girar_texto(text):
+"""def girar_texto(text):
 	
 	auxiliar = []
 	reverso = ""
@@ -10,6 +10,15 @@ def girar_texto(text):
 	for i in range(len(auxiliar)-1, -1, -1):
 		reverso = reverso + auxiliar[i]
 		
+	return reverso"""
+
+
+#Otra forma (mejor):
+def girar_texto(text):
+	reverso = ""
+	for i in text:
+		#Guardo en la cadena primero la i y luego concateno la cadena:
+		reverso = i + reverso
 	return reverso
-
-
+	
+print (girar_texto("hola"))
